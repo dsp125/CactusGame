1cc832a82db1d85e2bcf095e9b2c644ef0d0d71c
Dhruv Patel <dpatel0125@gmail.com>
2025-01-29 16:06:23 -0500

    Thorn Attack and Mouse controls

---
+++ Hitboxes/Projectiles/Scripts/Thorn.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+export var speed = 200
+var direction = Vector2.ZERO
+
+func _physics_process(delta):
+	direction = Vector2.RIGHT.rotated(rotation)
+	global_position += direction * speed * delta
+
+
+func destroy():
+	queue_free()
+	
+
+func _on_Area2D_area_entered(area):
+	print(area)
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_entered(body):
+	print(body)
+	pass # Replace with function body.
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	print("leaving")
+	pass # Replace with function body.
--- Player/Player.gd
+++ Player/Player.gd
@@ -3,19 +3,26 @@ extends KinematicBody2D
 const ACCEL = 400
 const MAX_SPD = 60
 const ROLL_SPEED = 75
+const ATTACK_SPEED = 45
 const FRICTION = 500
 
 var velocity = Vector2.ZERO
-var roll_vector = Vector2.DOWN
+var prev_vector = Vector2.DOWN #USED TO SAVE LAST FACING DIRECTION
+var direction_vector = get_global_mouse_position() - position
 
+#Scenes instantiated via player script
+export var REF_THORN = preload("res://Hitboxes/Projectiles/Thorn.tscn")
+
+#Animation Controllers
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var thornSpawn = $ThornSpawn
 
 enum PlayerStates{
 	MOVE,
 	ROLL,
-	ATTACK
+	SHOOT
 }
 
 var state = PlayerStates.MOVE
@@ -28,41 +35,92 @@ func _process(delta):
 		PlayerStates.ROLL:
 			roll_state(delta)
 		
-		PlayerStates.ATTACK:
-			pass
+		PlayerStates.SHOOT:
+			shoot_state(delta)
 		
 func move_state(delta):
 	var input_vector = Vector2.ZERO
+	direction_vector = get_global_mouse_position() - position
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 		
 	if input_vector != Vector2.ZERO:
-		roll_vector = input_vector
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Walk/blend_position", input_vector)
-		animationTree.set("parameters/Roll/blend_position", input_vector)
+		prev_vector = input_vector
+		animationTree.set("parameters/Idle/blend_position", direction_vector)
+		animationTree.set("parameters/Walk/blend_position", direction_vector)
 		animationState.travel("Walk")
 		velocity = velocity.move_toward(input_vector * MAX_SPD, ACCEL * FRICTION * delta)
 		
 	else:
+		animationTree.set("parameters/Idle/blend_position", direction_vector)
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION*delta)
 	
 	move()
 	
 	if Input.is_action_just_pressed("ui_roll"):
-		animationTree.set("parameters/Roll/blend_position", roll_vector)
+		animationTree.set("parameters/Roll/blend_position", direction_vector)
 		state = PlayerStates.ROLL
+	
+	if Input.is_action_pressed("ui_attack"):
+		if input_vector != Vector2.ZERO:
+			animationTree.set("parameters/AttackWalk/blend_position", input_vector)
+		else:
+			animationTree.set("parameters/AttackIdle/blend_position", direction_vector)
+		state = PlayerStates.SHOOT
 		
-
+# ROLLING
 func roll_state(_delta):
-	velocity = roll_vector * ROLL_SPEED
+	velocity = prev_vector * ROLL_SPEED
 	animationState.travel("Roll")
 	move()
 
 func roll_animation_finished():
 	state = PlayerStates.MOVE
 
+# SHOOTING THORNS
+func shoot_thorn():
+	print("shooting thorn")
+	if REF_THORN:
+		var thorn = REF_THORN.instance()
+		get_tree().current_scene.add_child(thorn)
+		thorn.global_position = thornSpawn.global_position
+		thorn.rotation = direction_vector.angle()
+		
+		#var thorn_rotation
+	
+func shoot_state(_delta):
+	direction_vector = get_global_mouse_position() - position
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+		
+	if input_vector != Vector2.ZERO:
+		prev_vector = input_vector
+		animationTree.set("parameters/AttackIdle/blend_position", direction_vector)
+		animationTree.set("parameters/AttackWalk/blend_position", direction_vector)
+		animationTree.set("parameters/Roll/blend_position", direction_vector)
+		animationTree.set("parameters/Idle/blend_position", direction_vector)
+		animationTree.set("parameters/Walk/blend_position", direction_vector)
+		animationState.travel("AttackWalk")
+		velocity = velocity.move_toward(input_vector * ATTACK_SPEED, ACCEL * FRICTION * _delta)
+		
+	else:
+		animationTree.set("parameters/AttackIdle/blend_position", direction_vector)
+		animationState.travel("AttackIdle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION*_delta)
+	
+	move()
+	
+	if Input.is_action_just_pressed("ui_roll"):
+		animationTree.set("parameters/Roll/blend_position", direction_vector)
+		state = PlayerStates.ROLL
+	
+	if Input.is_action_just_released("ui_attack"):
+		state = PlayerStates.MOVE
+
+# UPDATE PLAYER POSITION
 func move():
 	velocity = move_and_slide(velocity)
