6726f573d5ea82cd8abc97a256b99ae15575a1a8
Dhruv Patel <dpatel0125@gmail.com>
2025-01-29 21:25:13 -0500

    Enemy stat updates

--- Enemies/Scripts/BearBandito.gd
+++ Enemies/Scripts/BearBandito.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 export var ACCELERATION = 300
-export var MAX_SPEED = 50
+export var MAX_SPEED = 20
 export var FRICTION = 200
 
 enum {
@@ -15,23 +15,32 @@ var velocity = Vector2.ZERO
 var state = IDLE
 onready var stats = $Stats
 onready var player_detection_zone = $PlayerDetectionZone
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO,200 * delta)
 	knockback = move_and_slide(knockback)
-
+	
 	match state:
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
-			
 		PATROL:
 			pass
 		CHASE:
 			var player = player_detection_zone.player
 			if player != null:
 				var direction = (player.global_position - global_position).normalized()
+				animationTree.set("parameters/Patrol/blend_position", direction)
+				animationState.travel("Patrol")
 				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			if player == null:
+				animationState.travel("Idle")
+				velocity = Vector2.ZERO
+				state = IDLE
 		
 	velocity = move_and_slide(velocity)
 
